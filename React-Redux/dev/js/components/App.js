7847b490aa4f62554a28f62f7ede99d3144df5ac
dowinweb <514435526@qq.com>
2017-03-13 11:40:27 +0800

    learn redux

---
+++ React-Redux/dev/js/components/App.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dongwei on 2017/3/12.
+ */
+import React from 'react';
+import UserList from '../containers/user-list';
+require('../../scss/style.scss');
+
+const App = () => (
+    <div>
+        <h2>Username List:</h2>
+        <UserList />
+        <hr/>
+        <h2>User Details</h2>
+    </div>
+);
+
+
+export default App;
