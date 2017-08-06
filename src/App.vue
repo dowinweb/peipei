aa7b95f6ee6e79d5b447213cfd89313ab28e3057
dowinweb <514435526@qq.com>
2017-08-06 22:02:06 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+    <app-footer v-bind:title="title"></app-footer>
+    <ninjas v-bind:ninjas="ninjas" ></ninjas>
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+
+export default {
+  components:{
+    'app-header':Header,
+    'app-footer':Footer,
+    'ninjas':Ninjas
+  },
+  data () {
+    return {
+      title: 'Vue Ninja app',
+      ninjas: [
+          {name: 'Ryu', speciality: 'Vue Components', show: false},
+          {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+          {name: 'Hitoshi', speciality: 'Click Events', show: false},
+          {name: 'Tango', speciality: 'Conditionals', show: false},
+          {name: 'Kami', speciality: 'Webpack', show: false},
+          {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
+    }
+  },
+  methods:{
+    updateTitle:function(updateTitle){
+      this.title = updateTitle
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+  color:purple;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1 v-on:click="changeTitle">
+      {{title}}
+    </h1>
+  </header>
+</template>
+
+<script>
+import {bus} from '../main';
+export default {
+  props:{
+    title:{
+      type:String
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+    changeTitle:function(){
+      // this.$emit('changeTitle','Vue wizards')
+      bus.$emit('titleChanged','Vue wizards')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header{
+    background: lightgreen;
+    padding:10px;
+  }
+  h1{
+    color:#222;
+    text-align: center;
+  }
+</style>
